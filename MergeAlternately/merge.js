d4fcd1bb74076c48f391097de1805d4ca837a074
Randy Luc <113475637+randi801j@users.noreply.github.com>
2023-08-01 18:40:04 -0700

    Update merge.js

--- MergeAlternately/merge.js
+++ MergeAlternately/merge.js
@@ -1,8 +1,9 @@
 var merge = function (word1,word2){
     let result="";
-    for (let i=0;i<Math.max(word1.length,word2.length);i++){
+    let range = Math.max(word1.length,word2.length); // Math.mac gives maximum value. If word one has 6 characters and word 2 has 7 it will make range 7
+    for (let i= 0; i<range; i++) { // this sets i at 0 then range at 7 due to Math.Max function
         for(i<word1.length)
-            result+= word1[i];
+            result+= word1[i];// takes the first letter of the word and merge it with word2
         for(i<word2.length) 
             result+=word2[i];
     }
